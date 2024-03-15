ca03fefe1166aca9d9e4e06a4e07dd6598872f39
jusvel <justinas.veligoras@gmail.com>
2024-03-15 15:26:55 +0200

    fixed auth check

--- src/components/DefaultLayout.jsx
+++ src/components/DefaultLayout.jsx
@@ -1,10 +1,31 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 import axiosClient from "../axiosClient";
 
 export default function DefaultLayout() {
+  const navigate = useNavigate();
+  useEffect(() => {
+    const checkLoggedIn = async () => {
+      try {
+        const response = await axiosClient.get("/users/verify", {
+          withCredentials: true,
+        });
+
+        if (response.status === 200) {
+          console.log("User is logged in");
+        }
+      } catch (error) {
+        if (error.response.status === 401) {
+          navigate("/login");
+        }
+      }
+    };
+    checkLoggedIn();
+  }, []);
+
   const handleLogout = () => {
     axiosClient.get("/users/logout", { withCredentials: true }).then(() => {
-      window.location.href = "/login";
+      navigate("/login");
     });
   };
 
--- src/components/GuestLayout.jsx
+++ src/components/GuestLayout.jsx
@@ -1,32 +1,28 @@
-import { Navigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { Outlet } from "react-router-dom";
-import { useEffect, useState } from "react";
-import axios from "axios";
+import { useEffect } from "react";
+import axiosClient from "../axiosClient";
 
 export default function GuestLayout() {
-  const [loggedIn, setLoggedIn] = useState(false);
+  const navigate = useNavigate();
   useEffect(() => {
-    // Check if user is logged in
     const checkLoggedIn = async () => {
       try {
-        const response = await axios.get(
-          "http://localhost:3000/api/v1/users/verify",
-          { withCredentials: true }
-        );
+        const response = await axiosClient.get("/users/verify", {
+          withCredentials: true,
+        });
 
         if (response.status === 200) {
-          setLoggedIn(true);
           console.log("User is logged in");
         }
       } catch (error) {
-        console.error("Error checking user login status:", error.message);
+        if (error.response.status === 401) {
+          navigate("/login");
+        }
       }
     };
-
     checkLoggedIn();
   }, []);
-
-  if (loggedIn) return <Navigate to="/test" />;
 
   return (
     <div>
